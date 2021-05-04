1001f8a133e29d09dc39d1a741ee6a1ed299e14d
Dom Lanza <60485299+domlanza@users.noreply.github.com>
2021-05-04 19:16:23 -0400

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,167 @@
+﻿<%--Dominic Lanza
+Professor Pascucci 
+Server Side
+02/26/2021--%>
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PizzaProject.Default" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Dom&Nicks Pizza Shop</title>
+    <link href="PizzaStyle.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+    <%--    Heading outside of Form--%>
+    <h2>Benvenuto</h2>
+    <h3 id="WelcomeH">Welcome to Dom&Nicks Gourmet Pizza</h3>
+
+    <%--    Beginning of Form--%>
+    <form id="frmPizzaShop" runat="server">
+
+        <%--        Image for Pizza Shop--%>
+        <asp:Image ID="Image1" ImageUrl="~/Images/Dom&Nicks.jpg" runat="server" />
+
+        <%--        Div for input of Username, Address, Phone Number--%>
+        <div>
+            <p>
+                Please input your name:
+                <asp:TextBox ID="TextBoxUsername" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                Please input your address:
+                <asp:TextBox ID="TextBoxAddress" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                Please input your Phone Number:
+                <asp:TextBox ID="TextBoxPhoneNumber" runat="server"></asp:TextBox>
+            </p>
+        </div>
+        <%--        Div for delivery preference--%>
+        <div>
+            <p>
+                <b>Please select your delivery option:</b>
+                <asp:RadioButtonList ID="RadioButtonListPickup" runat="server" OnSelectedIndexChanged="RadioButtonListPickup_SelectedIndexChanged">
+                    <asp:ListItem Value="Delivery">Delivery</asp:ListItem>
+                    <asp:ListItem Value="Pickup">Pickup</asp:ListItem>
+                </asp:RadioButtonList>
+            </p>
+        </div>
+        <%--        Help labels for user assistance--%>
+        <h3>
+            <asp:Label ID="lbHint" runat="server" Text="Please select at least one pizza."></asp:Label></h3>
+
+        <p>
+            <asp:Label ID="lblCheckboxHelp" runat="server" Text="" Visible="false"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="lblQuantityHelp" runat="server" Text="" Visible="false"></asp:Label>
+        </p>
+
+        <%--        First gridview which is all the data to order a pizza from Pizza.DBO--%>
+        <div>
+            <asp:GridView ID="gv_Input" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:TemplateField HeaderText="Select">
+                        <ItemTemplate>
+                            <asp:CheckBox ID="CheckboxSelect" runat="server" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:BoundField DataField="PizzaType" HeaderText="Pizza Type" />
+                    <asp:TemplateField HeaderText="Size">
+                        <ItemTemplate>
+                            <asp:DropDownList ID="ddlSize" runat="server">
+                                <asp:ListItem Value="small" Text="Small"></asp:ListItem>
+                                <asp:ListItem Value="medium" Text="Medium"></asp:ListItem>
+                                <asp:ListItem Value="large" Text="Large"></asp:ListItem>
+                            </asp:DropDownList>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Quantity">
+                        <ItemTemplate>
+                            <asp:TextBox ID="TextboxQuantity" runat="server" type="number"></asp:TextBox>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+        <div>
+        </div>
+
+        <br>
+        <%--        Label for Name, Address, and Phone--%>
+        <asp:Label ID="lblNameAddressPhone" runat="server" Text="Label"></asp:Label>
+        <div>
+            <br>
+
+            <%--        Second gridview which is the data that user has selected to order--%>
+            <asp:GridView ID="gv_Output" runat="server" AutoGenerateColumns="False" ShowFooter="true">
+                <Columns>
+                    <asp:BoundField DataField="PizzaType" HeaderText="Pizza Type" />
+                    <asp:BoundField DataField="Size" HeaderText="Size" />
+                    <asp:BoundField DataField="Quantity" HeaderText="Quantity" />
+                    <asp:BoundField DataField="Price" HeaderText="Price" />
+                    <asp:BoundField DataField="TotalCost" HeaderText="Total Cost" />
+                </Columns>
+            </asp:GridView>
+
+            <%-- Label for gridview  --%>
+
+            <br>
+            <asp:Label ID="gv_totalSalesLabels" runat="server" Text="Total Sales" Visible="false"></asp:Label>
+            <br>
+            <%-- Total Sales Gridview --%>
+
+            <asp:GridView ID="gv_TotalSales" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="PizzaType" HeaderText="Pizza Type" />
+                    <asp:BoundField DataField="TotalSales" HeaderText="Total Sales" />
+                </Columns>
+            </asp:GridView>
+        </div>
+        <br>
+
+        <%-- Label for Total Quantity Gridview --%>
+
+        <asp:Label ID="gv_TotalQuantityLabel" runat="server" Text="Total Quantity" Visible="false"></asp:Label>
+        <br>
+        <%-- Total Quantity Gridview --%>
+
+        <div>
+            <asp:GridView ID="gv_TotalQuantity" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="PizzaType" HeaderText="Pizza Type" />
+                    <asp:BoundField DataField="TotalQuantityOrdered" HeaderText="Total Quantity" />
+                </Columns>
+            </asp:GridView>
+
+        </div>
+        <%-- Submit button --%>
+
+        <div>
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+        </div>
+        <br>
+        <%-- Back to order button --%>
+
+        <div>
+            <asp:Button ID="btnBackToOrder" runat="server" Text="Back To Order" OnClick="btnBackToOrder_Click" />
+        </div>
+        <br>
+        <%-- Show total Sales and Quantity tbutton --%>
+
+        <div>
+            <asp:Button ID="btnShowTotalSalesAndQuantity" runat="server" Text="Show Total Quantity and Sales" OnClick="btnShowTotalSalesAndQuantity_Click" />
+        </div>
+        <br>
+        <%-- Hide total Sales and Quantity button --%>
+
+        <div>
+            <asp:Button ID="btnHideTotalSalesAndQauntity" runat="server" Text="Hide Total Quantity and Sales" OnClick="btnHideTotalSalesAndQauntity_Click" />
+        </div>
+        <br>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Utilities;
+using PizzaLibrary;
+using System.Data;
+using System.Collections;
+
+namespace PizzaProject
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        //ArrayList for Pizza
+        List<Pizza> pizzaList = new List<Pizza>();
+        //Connect to DB
+        DBConnect dBConnect = new DBConnect();
+        //Call Order class to create new order
+        Order order = new Order();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ShowInput(true);
+                displayInputData();
+                lblNameAddressPhone.Visible = false;
+                btnBackToOrder.Visible = false;
+                btnShowTotalSalesAndQuantity.Visible = false;
+                btnHideTotalSalesAndQauntity.Visible = false;
+                lblCheckboxHelp.Visible = true;
+                lblQuantityHelp.Text = "";
+                lblCheckboxHelp.Text = "";
+            }
+        }
+      
+        public void ShowInput(Boolean tf)
+        {
+            lbHint.Visible = tf;
+            gv_Input.Visible = tf;
+        }
+        
+        public void displayInputData()
+        {
+            String str = "SELECT * FROM Pizza";
+            DataSet mydata = dBConnect.GetDataSet(str);
+
+            gv_Input.DataSource = mydata;
+            gv_Input.DataBind();
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Name Validation
+            String username = TextBoxUsername.Text;
+            if (username == "")
+            {
+                Response.Write("<script>alert('Type in your Name')</script>");
+            }
+
+            //Address Validation
+            String address = TextBoxAddress.Text;
+            if (address == "")
+            {
+                Response.Write("<script>alert('Type in your Address')</script>");
+            }
+
+            //Phone Number Validation
+            String phonenumber = TextBoxPhoneNumber.Text;
+            if (phonenumber == "")
+            {
+                Response.Write("<script>alert('Type in your Phone Number')</script>");
+            }
+
+            //Delivery or Pickup Validation
+            if (RadioButtonListPickup.SelectedValue != "Delivery" && RadioButtonListPickup.SelectedValue != "Pickup")
+            {
+                Response.Write("<script>alert('Please check Delivery option')</script>");
+            }
+            
+            //total price variable
+            double totPrice = 0;
+            //count checkboxes
+            int count = 0;
+        
+            //beginning checking rows in input gridView
+            for (int row = 0; row < gv_Input.Rows.Count; row++)
+            {
+                CheckBox Cbox = (CheckBox)gv_Input.Rows[row].FindControl("CheckboxSelect");
+                TextBox Tbox = (TextBox)gv_Input.Rows[row].FindControl("TextboxQuantity");
+
+                //If Checkbox is checked
+                if (Cbox.Checked)
+                {
+                    //Check if Tbox is null, if it is, Display error msg.
+                    if (Tbox.Text == null || string.IsNullOrEmpty(Tbox.Text) == true)
+                    {
+                        lblQuantityHelp.Visible = true;
+                        lblQuantityHelp.Text = "Please input a Quantity to proceed";
+                    }
+                    //if Textbox with quantity is filled proceed
+                    else
+                    {
+                        //create the new pizza object
+                        Pizza objPizza = new Pizza();
+
+                        //Hide griw view input
+                        gv_Input.Visible = false;
+
+                        //add to the pizza object
+                        string pizzaTypeGv = gv_Input.Rows[row].Cells[1].Text;
+                        objPizza.PizzaType = pizzaTypeGv;
+
+                        DropDownList PizzaSize = (DropDownList)gv_Input.Rows[row].FindControl("ddlSize");
+                        objPizza.Size = PizzaSize.SelectedValue.ToString();
+
+                        int pizzaQuantity = Convert.ToInt32(Tbox.Text);
+                     
+                        //int pizzaQuantity = Convert.ToInt32(Tbox.Text);
+                        objPizza.Quantity = pizzaQuantity;
+
+                        double PizzaPrice = Order.getPrice(objPizza.PizzaType, objPizza.Size);
+                        objPizza.Price = PizzaPrice;
+
+                        double TotalCostGV = (PizzaPrice * pizzaQuantity);
+                        objPizza.TotalCost = TotalCostGV;
+
+                        totPrice += TotalCostGV;
+
+                        //add pizza to the array list
+                        pizzaList.Add(objPizza);
+
+                        //Hide Name box, Address, PhoneNumber
+                        TextBoxUsername.Visible = false;
+                        TextBoxAddress.Visible = false;
+                        TextBoxPhoneNumber.Visible = false;
+                        //Display Name Address Phone input
+                        lblNameAddressPhone.Visible = true;
+                        lblNameAddressPhone.Text = "Name: " + username + " Phone Number: " + phonenumber + " Address: " + address;
+
+                        //Move data to gv_Output
+                        gv_Output.DataSource = pizzaList;
+                        gv_Output.Columns[0].FooterText = "Totals:";
+                        gv_Output.Columns[4].FooterText = totPrice.ToString();
+
+                        gv_Output.DataBind();
+                        //make gv_ouput visible
+                        gv_Output.Visible = true;
+
+                        //update database
+                        Order.updateDB(pizzaTypeGv, pizzaQuantity, PizzaPrice);
+
+                        ///Populate the gv_Total Sales with records
+                        String str = "SELECT PizzaType, TotalSales FROM Pizza ORDER BY TotalSales DESC";
+                        DataSet TotalSalesGV = dBConnect.GetDataSet(str);
+
+                        gv_TotalSales.DataSource = TotalSalesGV;
+                        gv_TotalSales.DataBind();
+
+
+                        ////populate the Total Quantity Gridview with records
+
+                        String str2 = "SELECT PizzaType, TotalQuantityOrdered FROM Pizza ORDER BY TotalQuantityOrdered DESC";
+                        DataSet TotalQuantityGV = dBConnect.GetDataSet(str2);
+
+                        gv_TotalQuantity.DataSource = TotalQuantityGV;
+                        gv_TotalQuantity.DataBind();
+
+                        //Hide the buttons
+                        btnBackToOrder.Visible = true;
+                        btnSubmit.Visible = false;
+                        btnShowTotalSalesAndQuantity.Visible = true;
+
+                        //Hide the gridviews
+                        gv_TotalQuantity.Visible = false;
+                        gv_TotalSales.Visible = false;
+
+                        //Controls hide
+                        TextBoxUsername.Visible = false;
+                        TextBoxPhoneNumber.Visible = false;
+                        TextBoxAddress.Visible = false;
+                        RadioButtonListPickup.Visible = false;
+                        lbHint.Visible = false;
+                        lblCheckboxHelp.Text = "";
+
+                        //increment count
+                        count = count + 1;
+                    }
+                }                
+                }//end of for loop
+            //If Checkbox is not checked display error message
+            if (count == 0) {
+                lblCheckboxHelp.Visible = true;
+                lblCheckboxHelp.Text = "You must select one pizza";
+                lblCheckboxHelp.Focus();
+                return;
+            }
+        }
+
+
+        protected void RadioButtonListPickup_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBackToOrder_Click(object sender, EventArgs e)
+        {
+            //Visibility controls to show first screen
+            lblCheckboxHelp.Visible = false;
+            lblQuantityHelp.Visible = false;
+            lblQuantityHelp.Text = "";
+            btnBackToOrder.Visible = false;
+            btnSubmit.Visible = true;
+            gv_Input.Visible = true;
+            gv_Output.Visible = false;
+            gv_TotalQuantity.Visible = false;
+            gv_TotalSales.Visible = false;
+
+            TextBoxUsername.Visible = true;
+            TextBoxPhoneNumber.Visible = true;
+            TextBoxAddress.Visible = true;
+            RadioButtonListPickup.Visible = true;
+            lbHint.Visible = true;
+        }
+
+        protected void btnShowTotalSalesAndQuantity_Click(object sender, EventArgs e)
+        {
+            //Display gridviews
+            gv_TotalQuantity.Visible = true;
+            gv_TotalSales.Visible = true;
+            btnShowTotalSalesAndQuantity.Visible = false;
+            btnHideTotalSalesAndQauntity.Visible = true;
+            gv_TotalQuantityLabel.Visible = true;
+            gv_totalSalesLabels.Visible = true;
+
+        }
+
+        protected void btnHideTotalSalesAndQauntity_Click(object sender, EventArgs e)
+        {
+            //Hide grid views
+            gv_TotalQuantity.Visible = false;
+            gv_TotalSales.Visible = false;
+            btnShowTotalSalesAndQuantity.Visible = true;
+            btnHideTotalSalesAndQauntity.Visible = false;
+            gv_TotalQuantityLabel.Visible = false;
+            gv_totalSalesLabels.Visible = false;
+
+        }
+    }
+}
